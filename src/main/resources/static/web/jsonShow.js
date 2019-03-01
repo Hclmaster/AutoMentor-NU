f9156cb7d78ef9a4e20c710823b2fa1dca1a6532
Chenghong Lin <chenghonglin2020@u.northwestern.edu>
2019-03-01 13:35:16 -0600

    Add more examples

--- src/main/resources/static/web/jsonShow.js
+++ src/main/resources/static/web/jsonShow.js
@@ -38,11 +38,35 @@ var jsonObj = new Vue({
             "textBlocks":[
                 {
                     "label":"square.rat",
-                    "text": "function overlay is not defined even though I have imported all the required packages",
+                    "text": "(interpolate-colors blue red 0.5)",
                     "type":"sourceCode"
+                },
+                {
+                    "label":"preprocessor",
+                    "text": "blue: this variable is not defined.",
+                    "type":"computerOutput"
                 }
             ],
-            "message":"I’m getting an error when I try to use function overlay",
+            "message":"I’m getting an error when I try to use function interpolate-colors",
+            "student":{
+                "name": "Cathy",
+                "id": "clj8621"
+            }
+        },
+        message4: {
+            "textBlocks":[
+                {
+                    "label":"square.rat",
+                    "text": "(overlay/xy main-flag 90 90 beside my-star beside my-star beside my-star)",
+                    "type":"sourceCode"
+                },
+                {
+                    "label":"preprocessor",
+                    "text": "overlay/xy: expects only 4 arguments, but found 11.",
+                    "type":"computerOutput"
+                }
+            ],
+            "message":"I’m getting an error when I try to use function overlay/xy",
             "student":{
                 "name": "Cathy",
                 "id": "clj8621"
